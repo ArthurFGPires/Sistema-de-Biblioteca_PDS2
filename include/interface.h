4b4c343dca74ed470f07937b83eb5a564d69953d
joaovitorb <joaovitorbc3033@gmail.com>
2023-10-26 10:45:36 -0300

    modificações e correções da interface

---
+++ include/interface.h
@@ -0,0 +1,29 @@
+#ifndef __INTERFACE_H__
+#define __INTERFACE_H__
+#include "biblioteca.h""
+#include "usuario.h""
+#include "funcionario.h""
+
+class Interface {
+
+public:
+    // Construtor da interface gráfica
+    Interface();
+    
+    // Área q permite o usuário logar ou abrir seção de cadastro de usuário 
+     void areaLogin();
+
+    // Menu onde o usuário pode ver os livros que estão emprestados a ele, pedir para listar livros disponíveis na biblioteca, pedir empréstimo de livro, devolver livro e verificar suas notificações 
+    void menuUsuario();
+
+    // Menu onde o funcionário pode adicionar ou remover livros a biblioteca e adicionar novos funcionários
+    void menuFuncionario();
+
+     // Menu de cadastro de um usuário 
+     shared_ptr<Usuario> cadastroUsuario();
+
+    // Menu de cadastro de um funcionário 
+    shared_ptr<Funcionario> cadastroFuncionario();
+};
+
+#endif
